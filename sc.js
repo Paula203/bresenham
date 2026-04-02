a1c2d62878d2559743f638c18661673883839676
Paula <est.mariap.amaya@unimilitar.edu.co>
2026-04-02 13:41:06 -0500

    Dibujar los ejes, el eje x y el eje y

--- sc.js
+++ sc.js
@@ -4,9 +4,19 @@ const escala = 20; // Tamaño de cada celda en píxeles
 
 /**
  * Dibuja un punto en el canvas 
- * @param {number} x - Coordenada x del punto
- * @param {number} y - Coordenada y del punto
+ * @param {number} x 
+ * @param {number} y 
  */
 function dibujarPunto(x, y) {
     ctx.fillRect(x * escala, canvas.height - y * escala, escala, escala);
 }
+
+function dibujarEjes() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+
+    ctx.moveTo(0, canvas.height);
+    ctx.lineTo(canvas.width, canvas.height);
+    
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, canvas.height);
