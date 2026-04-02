d08267cb59550e1473dede66a0da204d7428e5bd
Paula <est.mariap.amaya@unimilitar.edu.co>
2026-04-02 13:35:22 -0500

    corregi cosas

--- sc.js
+++ sc.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const escala = 20; // Tamaño de cada celda en píxeles
 
 /**
