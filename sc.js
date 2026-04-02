e224b76bf5f5f007acba017ede0e55941eb1d963
Paula <est.mariap.amaya@unimilitar.edu.co>
2026-04-02 13:33:58 -0500

    agrego funcion dibujarpunto para mostar los puntos en el canvas

--- sc.js
+++ sc.js
@@ -1,2 +1,12 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const escala = 20; // Tamaño de cada celda en píxeles
+
+/**
+ * Dibuja un punto en el canvas 
+ * @param {number} x - Coordenada x del punto
+ * @param {number} y - Coordenada y del punto
+ */
+function dibujarPunto(x, y) {
+    ctx.fillRect(x * escala, canvas.height - y * escala, escala, escala);
+}
